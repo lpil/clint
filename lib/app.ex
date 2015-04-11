8a56e957d5cc9bebbc4f45e685502d882909955b
Louis Pilfold <louis@lpil.uk>
2015-04-11 20:20:57 +0100

    html + eex render funcs

--- lib/app.ex
+++ lib/app.ex
@@ -5,6 +5,16 @@ defmodule App do
     conn
     |> text "Hello world!"
   end
+
+  get "/foo" do
+    conn
+    |> html "<h2>Hello html world!</h2>"
+  end
+
+  get "/foo/:name" do
+    conn
+    |> eex "<h2>Hello <%= name %>!</h2>", name: name
+  end
 end
 
 # min run -e App.start --no-halt
--- lib/clint/render.ex
+++ lib/clint/render.ex
@@ -4,14 +4,39 @@ defmodule Clint.Render do
 
   defmacro __using__(_) do
     quote do
-      import Clint.Render, only: [text: 2]
+      import Clint.Render, only: [
+        text: 2,
+        html: 2,
+        eex:  3,
+      ]
     end
   end
 
-  def text(conn, content) do
+  def text(conn, body) do
     status = conn.status || 200
     conn
     |> put_resp_content_type("text/plain")
-    |> send_resp(status, content)
+    |> send_resp(status, body)
+  end
+
+  def html(conn, body) do
+    status = conn.status || 200
+    IO.puts 1
+    conn
+    |> put_resp_content_type("text/html")
+    |> render(body)
+  end
+
+  def eex(conn, template, options \\ []) do
+    body = EEx.eval_string(template, options)
+    conn
+    |> html(body)
+  end
+
+
+  defp render(conn, body) do
+    status = conn.status || 200
+    conn
+    |> send_resp(status, body)
   end
 end
